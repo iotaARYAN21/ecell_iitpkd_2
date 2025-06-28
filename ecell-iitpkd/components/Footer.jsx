d6f32819f126b3cd24af50ab09c238790ff07503
aryan <aryan0312g@outlook.com>
2025-06-28 17:52:46 +0530

    changed the font size of main h1

--- ecell-iitpkd/components/Footer.jsx
+++ ecell-iitpkd/components/Footer.jsx
@@ -14,11 +14,11 @@ const Footer = () => {
     <footer>
         <div className="footer_div1">
             <div className="footer_r1">
-            <div className="ecell-logo-container ">
-                <img src="./lg.png" alt="" className="ecell-logo" /> 
-            </div>
-            <h3 >E-Cell IIT PALAKKAD</h3>
-            <h3>The Fire Within</h3>
+                <div className="ecell-logo-container ">
+                    <img src="./lg.png" alt="" className="ecell-logo" /> 
+                </div>
+                <h3 >E-Cell IIT PALAKKAD</h3>
+                <h3>The Fire Within</h3>
             </div>
             <div className="footer_r2">
                 <ul>
